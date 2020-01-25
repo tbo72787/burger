2463dc7dc9e03321e0ee098eae36370740f47b62
Taylor Zettel <tzettel@gmail.com>
2020-01-25 09:40:11 -0500

    created controllers folder; created burgers_controller.js inside controllers file, which contains and exports router

--- models/burger.js
+++ models/burger.js
@@ -3,18 +3,18 @@ var orm = require("../config/orm.js");
 
 var burger = {
   all: function(cb) {
-    orm.all("Burgers", function(res) {
+    orm.SelectAll("Burgers", function(res) {
       cb(res);
     });
   },
   // The variables cols and vals are arrays.
   create: function(cols, vals, cb) {
-    orm.create("Burgers", cols, vals, function(res) {
+    orm.insertOne("Burgers", cols, vals, function(res) {
       cb(res);
     });
   },
   update: function(objColVals, condition, cb) {
-    orm.update("Burgers", objColVals, condition, function(res) {
+    orm.updateOne("Burgers", objColVals, condition, function(res) {
       cb(res);
     });
   }
